494bd2d0401327c76130d1adfa43144cc457592e
lrlna <shestak.irina@gmail.com>
2016-07-06 12:29:21 +0100

    woops, put back the extra argv check

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ if (!argv.f || !argv.n || !argv._[0]) {
   process.exit()
 }
 
+if (!argv.f || !argv.n || !argv._[0]) {
+  require('yargs').showHelp()
+  process.exit()
+}
+
 var xray = Xray()
 var file = argv.f
 var yourName = argv.n
